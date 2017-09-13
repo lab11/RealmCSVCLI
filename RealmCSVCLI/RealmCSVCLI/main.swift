a005f3f18c0909f6993a5614d5a5bb401e09f9e5
Noah <nklugman@Noahs-MacBook-Pro.local>
2017-09-13 10:09:06 -0700

    adding in build changes so it doesn't depend so heavily on local paths

--- RealmCSVCLI/RealmCSVCLI/main.swift
+++ RealmCSVCLI/RealmCSVCLI/main.swift
@@ -7,6 +7,12 @@
 //
 
 import Foundation
+import RealmConverter
+
 
 print("Hello, World!")
+let realmFilePath = "/Users/noah/Desktop/Phone_Data/21_adballah_J/realm_to_convert/realm_a4e2275e0ba8aff2_20170326_1827.realm" // Absolute file path to my Realm file
+let outputFolderPath = "/Users/noah/Desktop/" // Absolute path to the folder which will hold the CSV files
 
+let csvDataExporter = try! CSVDataExporter(realmFilePath: realmFilePath)
+try! csvDataExporter.export(toFolderAtPath: outputFolderPath)
